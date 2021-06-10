2f9e2ece79b441e71f60492be9420023b1f772ff
Catrionamcd <catrionamcd@gmail.com>
2021-06-10 14:23:58 +0000

    http.server

--- assets/js/script.js
+++ assets/js/script.js
@@ -16,13 +16,22 @@ document.addEventListener("DOMContentLoaded", function() {
       })
     } 
     
-    runGame("addition");
+    document.getElementById("answer-box").addEventListener("keydown", function(event) {
+        if (event.key === "Enter") {
+            checkAnswer();
+        }
+    })
+
+        runGame("addition");
 })
 /**
  * The main game loop, called when the script is first loaded & after
  * the user's answer has been processed
  */
 function runGame(gameType) {
+
+    document.getElementById("answer-box").value = "";
+    document.getElementById("answer-box").focus();
 
     let num1 = Math.floor(Math.random() * 25) + 1;
     let num2 = Math.floor(Math.random() * 25) + 1;
